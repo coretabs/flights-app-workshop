d110a559fc4eeb0725d3aa1d10181e6bc98a0b7d
Asmaa Salih <37132467+asmaasalih@users.noreply.github.com>
2020-02-01 20:10:20 +0400

    adding models

--- airlines/travel/admin.py
+++ airlines/travel/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Plane, Flight, Passenger, Ticket, User_Info 
 # Register your models here.
+admin.site.register(Plane)
+admin.site.register(Passenger)
+admin.site.register(Flight)
+admin.site.register(Ticket)
+admin.site.register(User_Info)
--- airlines/travel/models.py
+++ airlines/travel/models.py
@@ -1,3 +1,55 @@
 from django.db import models
+import time
+import datetime
+from django.utils import timezone
+from django.core.exceptions import ValidationError
 
-# Create your models here.
+
+class Plane(models.Model):
+    name = models.CharField(max_length=50,unique=True)
+    num_of_seats = models.IntegerField()
+    available = models.BooleanField(default=True)
+
+    def __str__(self):                           
+	    return self.name   
+
+def present_or_future_date(value):
+    if value < timezone.now():
+        raise ValidationError("The date cannot be in the past!")
+    return value
+
+class Flight(models.Model):
+    plane_name = models.ForeignKey(Plane,on_delete=models.CASCADE)
+    from_city = models.CharField(max_length=50)
+    to_city = models.CharField(max_length=50)
+    from_time = models.DateTimeField(validators=[present_or_future_date])
+    to_time = models.TimeField()
+
+    def __str__(self):
+        return '{} - {}'.format(self.from_city,self.to_city)
+
+    
+        
+
+class Passenger(models.Model):
+    full_name = models.CharField(max_length=100)
+    age = models.IntegerField()
+    GENDER_CHOICES = [('M','Male'),('F','Female')]
+    gender = models.CharField(choices=GENDER_CHOICES,max_length=1)
+
+    def __str__(self):
+        return self.full_name
+
+class Ticket(models.Model):
+    passenger = models.ForeignKey(Passenger,on_delete=models.CASCADE)
+    flight = models.ForeignKey(Flight,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return '{}  --> ({})'.format(self.passenger,self.flight)
+
+class User_Info(models.Model):
+    name = models.CharField(max_length=50)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.name
